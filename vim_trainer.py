a34c41371fbd5efecaf8a59ecd2ddf79a0cf9bad
torhen <torhen@gmail.com>
2020-01-17 10:38:12 +0000

    changed vim_trainer.py

--- vim_trainer.py
+++ vim_trainer.py
@@ -2,11 +2,16 @@ import os
 quote="""Who the fuck is Alice?"""
 
 def exam_01(quote):
+    task = 'replace letters (r)'
     res =quote
     for c in ['a', 'e', 'i', 'o', 'u']:
         res = res.replace(c, '_')
-    with open('test.txt', 'w') as fin:
-            fin.write(res)
-exam_01(quote)
+    return f'{task}\n{res}'
+
+txt = f'quote: {quote}\n\n'
+txt = txt + exam_01(quote)
+
+with open('test.txt', 'w') as fin:
+            fin.write(txt)
 os.system('vim test.txt')
 
